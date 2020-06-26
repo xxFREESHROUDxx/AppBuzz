1794965a996a2964d047f7c17c91ba2fa0181b39
SudeepMi <sudeepm0599@gmail.com>
2020-06-26 15:11:30 +0545

    version 1.2

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 const app = require("./src/app");
-const path = require("path");
 
-//serve static asset 
-if(process.env.NODE_ENV==="production"){
-
-  app.use(express.static('client/build'));
-  app.get('*',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 
 
 const port = process.env.PORT || 8080;
