0e8ba8e6d29f7fef0d99ac8211b4cbfeb2f8b71b
SudeepMi <sudeepm0599@gmail.com>
2020-06-27 10:55:15 +0545

    index

--- client/src/push-notifications.js
+++ client/src/push-notifications.js
@@ -17,15 +17,14 @@ async function askUserPermission() {
  * shows a notification
  */
 function sendNotification() {
-  const img = "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg";
+  // const img = "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg";
   const text = "Take a look at this brand new t-shirt!";
-  const title = "New Product Available";
+  const title = "New Notifi";
   const options = {
     body: text,
     icon: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
     vibrate: [200, 100, 200],
     tag: "new-product",
-    image: img,
     badge: "https://spyna.it/icons/android-icon-192x192.png",
     actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
   };
@@ -61,11 +60,8 @@ async function createNotificationSubscription() {
  */
 async function getUserSubscription() {
   //wait for service worker installation to be ready, and then
-  console.log(navigator.serviceWorker);
-  if(isPushNotificationSupported){
-    console.log(navigator)
-    return await navigator.serviceWorker.ready
-    .then(function(serviceWorker) {
+  if(registerServiceWorker){
+    return await navigator.serviceWorker.ready.then(function(serviceWorker) {
       return serviceWorker.pushManager.getSubscription();
     })
     .then(function(pushSubscription) {
