31e0ed5c7bb5b6aad253be302554dd11b11aaa60
SudeepMi <sudeepm0599@gmail.com>
2020-06-29 13:14:28 +0545

    v3

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,33 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+
+addBtn.style.display = 'none';
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.log("here you are")
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
