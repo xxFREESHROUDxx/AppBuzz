5a069c2510105d19d56e2bf59faacf2322e4a692
xxFREESHROUDxx <baibhavkc11@gmail.com>
2022-02-01 11:34:05 +0545

    initial commit

--- client/src/components/Chat/Messages.js
+++ client/src/components/Chat/Messages.js
@@ -7,7 +7,7 @@ import Message from './Message/Message';
 import './Messages.css';
 
 const Messages = ({ messages, name }) => (
-  <ScrollToBottom className="messages" followButtonClassName="scrollbtn                                                     "	>
+  <ScrollToBottom className="messages" followButtonClassName="scrollbtn">
     {messages.map((message, i) => <div key={i} className="test"><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
 );
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8080;
-// const host = process.env.HOSTNAME || "codewithsudeep";
+// const host = process.env.HOSTNAME || "xxfreeshroudxx";
 const host = process.env.HOSTNAME || "0.0.0.0";
 
 
 const server = app.listen(port, host, () => {
-  console.log('Port %d',port);
+  console.log('Port %d', port);
   console.log(`Node.js API server is listening on http://${host}`);
 });
 
@@ -33,47 +33,51 @@ const path = require("path");
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./src/users');
 
 
-
+// Initializing socket.io connection
 io.on('connect', (socket) => {
   console.log("user connected")
+  //For the new user joining the room
   socket.on('join', ({ name, room }, callback) => {
+    //Create user
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+    if (error) return callback(error);
 
     socket.join(user.room);
-
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    //Displays a welcome message to the user who have joined the room.
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+    //displays a joined room message to all other room users except that particular user
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     callback();
   });
-
+  //User sending message to the room.
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit('message', { user: user.name, text: message });
-    
+
     callback();
   });
 
-  socket.on('sendTyping', (data)=>{
+  socket.on('sendTyping', (data) => {
     const user = getUser(socket.id);
-    if(data.typing==true){
-       io.to(user.room).emit('display', data)
+    if (data.typing == true) {
+      io.to(user.room).emit('display', data)
     }
-    
-  })
 
+  })
+  // When the user exits from the room
   socket.on('disconnect', () => {
+    //the user is deleted from array of users and a left room message displayed
     console.log("user dis")
     const user = removeUser(socket.id);
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
   })
 });
@@ -82,11 +86,11 @@ io.on('connect', (socket) => {
 //serve static asset 
 app.use(cors());
 // app.use(router);
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
 
   app.use(express.static('client/build'));
-  app.get('*',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
